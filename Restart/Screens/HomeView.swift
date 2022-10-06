2b87f8dab49ab14cdab0eb3335bd1dcdf438c968
alreis <al.reis@live.com>
2022-10-06 17:49:54 -0300

    Made the onboarding view page

---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Alysson Reis on 06/10/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Home")
+                .font(.largeTitle)
+            
+            Button(action: {
+                isOnboardingViewActive = true
+            }) {
+                Text("Restart")
+            }
+        } //: VSTACK
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
